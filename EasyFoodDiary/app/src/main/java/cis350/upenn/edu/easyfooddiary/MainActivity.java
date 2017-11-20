05578a65d10de2d4e92d6b00308c80406f08a1ef
chrisf <f010243@126.com>
2017-11-20 16:56:43 -0500

    add friends by username

--- EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/MainActivity.java
+++ EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/MainActivity.java
@@ -1,16 +1,57 @@
 package cis350.upenn.edu.easyfooddiary;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    protected NotificationCompat.Builder notification;
+    protected NotificationCompat.Builder notification_2;
+
+    private static final int uniqueID = 45612;
+    private static final int uniqueID2 = 45613;
+
+    //protected String value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setNotificationFitnessMinute();
+
+//        notification = new NotificationCompat.Builder(this);
+//        notification.setAutoCancel(false);
+//        notification.setSmallIcon(R.drawable.food_diary_title);
+//        notification.setTicker("This is the ticker");
+//        notification.setWhen(System.currentTimeMillis());
+//        notification.setContentTitle("This is the title");
+//        notification.setContentText(editText_minutesPerWorkout.getText().toString());
+//
+//        notification.setContentText("this is the context");
+//        Intent i = new Intent(this, MainActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+//        notification.setContentIntent(pendingIntent);
+//        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        nm.notify(uniqueID, notification.build());
+
     }
 
     public void onClick(View view) {
@@ -18,6 +59,16 @@ public class MainActivity extends AppCompatActivity {
         if (view.getId() == R.id.your_information) {
             Intent i = new Intent(this, InformationActivity.class);
             startActivity(i);
+//            notification.setSmallIcon(R.drawable.food_diary_title);
+//            notification.setTicker("This is the ticker");
+//            notification.setWhen(System.currentTimeMillis());
+//            notification.setContentTitle("This is the title");
+//            notification.setContentText("this is the context");
+//            Intent i = new Intent(this, MainActivity.class);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+//            notification.setContentIntent(pendingIntent);
+//            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//            nm.notify(uniqueID, notification.build());
         }
         else if (view.getId() == R.id.motivation) {
             Intent i = new Intent(this, MotivationActivity.class);
@@ -66,4 +117,41 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
     }
+
+
+
+
+    public void setNotificationFitnessMinute(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference myref_minutesPerWorkout = FirebaseDatabase.getInstance().getReference("Minutes_Per_Workout").child(user.getUid());
+
+        myref_minutesPerWorkout.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+
+                notification = new NotificationCompat.Builder(MainActivity.this);
+                notification.setAutoCancel(false);
+                notification.setSmallIcon(R.drawable.food_diary_title);
+                notification.setTicker("This is the ticker");
+                notification.setWhen(System.currentTimeMillis());
+                notification.setContentTitle("DESIRED MINUTES");
+                notification.setContentText("YOU NEED TO FINISH "+value+" MINUTES!!");
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+                notification.setContentIntent(pendingIntent);
+                NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                nm.notify(uniqueID, notification.build());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.w("tag", "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
+
 }
