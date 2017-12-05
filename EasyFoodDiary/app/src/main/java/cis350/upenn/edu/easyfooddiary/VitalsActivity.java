3fd69538308be534d94ca12326247e68ec967e23
Sean Lau <seanlau@vagelos-ve504-0192.apn.wlan.upenn.edu>
2017-12-05 15:31:09 -0500

    RealFinal

--- EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/VitalsActivity.java
+++ EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/VitalsActivity.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,16 +20,20 @@ import org.json.JSONException;
 /**
  * Created by Daniel on 3/24/2017.
  */
-    public class VitalsActivity extends AppCompatActivity {
+public class VitalsActivity extends AppCompatActivity {
 
     protected VitalsView vitalsView;
+
     protected String heartRate;
     protected String bloodPressureS;
     protected String bloodPressureD;
     protected String bodyTemperature;
     protected String respiratoryRate;
+
+    protected String date;
+    protected String monthyear;
     protected JSONArray dateInfo;
-    Date datebundle = new Date();
+
     protected EditText editText_heartRate;
     protected EditText editText_bloodPressureS;
     protected EditText editText_bloodPressureD;
@@ -43,15 +45,8 @@ import org.json.JSONException;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vitals);
         vitalsView = new VitalsView(this);
-        datebundle.setDate(getIntent().getExtras().getString("DATE"));
-        String date = datebundle.getDate();
-        datebundle.setMonthyear(getIntent().getExtras().getString("MONTHYEAR"));
-
-        //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        //DatabaseReference myref_date = FirebaseDatabase.getInstance().getReference(date + "v").child(user.getUid());
-
-
-
+        date = getIntent().getExtras().getString("DATE");
+        monthyear = getIntent().getExtras().getString("MONTHYEAR");
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myref_date = database.getReference(date + "v");
         myref_date.addValueEventListener(new ValueEventListener() {
@@ -64,8 +59,12 @@ import org.json.JSONException;
                     } else {
                         dateInfo = new JSONArray(s);
                     }
+                    editText_heartRate = (EditText) findViewById(R.id.hr);
+                    editText_bloodPressureS = (EditText) findViewById(R.id.bps);
+                    editText_bloodPressureD = (EditText) findViewById(R.id.bpd);
+                    editText_bodyTemperature = (EditText) findViewById(R.id.bt);
+                    editText_respiratoryRate = (EditText) findViewById(R.id.rr);
 
-                    getEditTexts();
                     editText_heartRate.setText((String) dateInfo.get(0));
                     editText_bloodPressureS.setText((String) dateInfo.get(1));
                     editText_bloodPressureD.setText((String) dateInfo.get(2));
@@ -77,20 +76,22 @@ import org.json.JSONException;
                 }
             }
             public void onCancelled(DatabaseError error) {
-                    // Failed to read value
-                    Log.w("tag", "Failed to read value.", error.toException());
+                // Failed to read value
+                Log.w("tag", "Failed to read value.", error.toException());
             }
         });
     }
 
     public void onClick(View view) {
-
-        //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        //DatabaseReference myref_date = FirebaseDatabase.getInstance().getReference(datebundle.getDate() + "s");
-
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myref_date = database.getReference(datebundle.getDate() + "s");
-        getEditTexts();
+        DatabaseReference myref_date = database.getReference(date + "v");
+
+        editText_heartRate = (EditText) findViewById(R.id.hr);
+        editText_bloodPressureS = (EditText) findViewById(R.id.bps);
+        editText_bloodPressureD = (EditText) findViewById(R.id.bpd);
+        editText_bodyTemperature = (EditText) findViewById(R.id.bt);
+        editText_respiratoryRate = (EditText) findViewById(R.id.rr);
+
         heartRate = editText_heartRate.getText().toString();
         bloodPressureS = editText_bloodPressureS.getText().toString();
         bloodPressureD = editText_bloodPressureD.getText().toString();
@@ -103,9 +104,6 @@ import org.json.JSONException;
             dateInfo.put(2, bloodPressureD);
             dateInfo.put(3, bodyTemperature);
             dateInfo.put(4, respiratoryRate);
-
-            //myref_date.child(user.getUid()).setValue(dateInfo.toString());
-
             myref_date.setValue(dateInfo.toString());
             Toast.makeText(vitalsView.getContext(),
                     "Saved", Toast.LENGTH_SHORT).show();
@@ -117,12 +115,4 @@ import org.json.JSONException;
             Toast.makeText(VitalsActivity.this, "Error2", Toast.LENGTH_SHORT).show();
         }
     }
-    public void getEditTexts(){
-        editText_heartRate = (EditText) findViewById(R.id.hr);
-        editText_bloodPressureS = (EditText) findViewById(R.id.bps);
-        editText_bloodPressureD = (EditText) findViewById(R.id.bpd);
-        editText_bodyTemperature = (EditText) findViewById(R.id.bt);
-        editText_respiratoryRate = (EditText) findViewById(R.id.rr);
-    }
 }
-
