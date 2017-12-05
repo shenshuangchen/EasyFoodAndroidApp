72a2fc1380686918f416488ad825a6faf10287df
chrisf <f010243@126.com>
2017-12-05 15:58:57 -0500

    notification

--- EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/MainActivity.java
+++ EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/MainActivity.java
@@ -1,17 +1,33 @@
 package cis350.upenn.edu.easyfooddiary;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    protected NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setNotificationFitnessMinute();
     }
 
     public void onClick(View view) {
@@ -87,5 +103,37 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(this, SocialActivity.class);
             startActivity(i);
         }
+    }
+
+    public void setNotificationFitnessMinute(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference myref_minutesPerWorkout = FirebaseDatabase.getInstance().getReference("Minutes_Per_Workout").child(user.getUid());
+
+        myref_minutesPerWorkout.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+
+                notification = new NotificationCompat.Builder(MainActivity.this);
+                notification.setAutoCancel(false);
+                notification.setSmallIcon(R.drawable.food_diary_title);
+                notification.setTicker("This is the ticker");
+                notification.setWhen(System.currentTimeMillis());
+                notification.setContentTitle("DESIRED MINUTES");
+                notification.setContentText("YOU NEED TO FINISH "+value+" MINUTES!!");
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+                notification.setContentIntent(pendingIntent);
+                NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                nm.notify(uniqueID, notification.build());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.w("tag", "Failed to read value.", databaseError.toException());
+            }
+        });
     }
 }
